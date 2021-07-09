d12d0facce5e30dfc0eab570375083cdc75218e4
Francisco Santos <francisco.santos.lx81@gmail.com>
2021-07-09 17:39:42 +0200

    Read / Write inputs

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 const list = document.querySelector("#players");
 
+// Append content
 list.insertAdjacentHTML("beforeend", "<li>Luke</li>");
 list.insertAdjacentHTML("beforeend", "<li>Anakin</li>");
 
-
+// Selecting several elements
 const winners = document.querySelectorAll("#fifa-wins li");
 console.log(winners);
 
@@ -15,5 +16,13 @@ const listWinners = document.getElementById("fifa-wins");
 
 setTimeout(() => {
   listWinners.insertAdjacentHTML("beforeend", "<li>France (2 wins)</li>");
-  listWinners.classList.add("red");
-}, 3000)
+  // Adding a class
+  listWinners.classList.add("red")
+}, 3000);
+
+// Read / Write inputs
+const emailInput = document.querySelector("#email");
+// Read
+console.log(emailInput.value);
+// Write
+emailInput.value = "francisco@gmail.com";
