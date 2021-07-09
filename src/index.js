26579f052c279f30d9252abc6bd8fbfe52f409ee
Francisco Santos <francisco.santos.lx81@gmail.com>
2021-07-09 16:19:05 +0200

    manipulated the DOM with several elements

--- src/index.js
+++ src/index.js
@@ -2,3 +2,14 @@ const list = document.querySelector("#players");
 
 list.insertAdjacentHTML("beforeend", "<li>Luke</li>");
 list.insertAdjacentHTML("beforeend", "<li>Anakin</li>");
+
+
+const winners = document.querySelectorAll("#fifa-wins li");
+console.log(winners);
+
+winners.forEach((winner) => {
+  console.log(winner.innerText);
+});
+
+const listWinners = document.getElementById("fifa-wins");
+listWinners.insertAdjacentHTML("beforeend", "<li>France (2 wins)</li>");
